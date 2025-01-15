6cc835eab54c6693239fea11083ddf3713c87106
JulienGln <juliengalerne.jg@gmail.com>
2025-01-15 09:21:32 +0100

    Create indexation.js

---
+++ script/indexation.js
@@ -0,0 +1,142 @@
+// Version corrigée
+
+const fs = require("fs");
+
+//------------------------------------------------------------MODELISATION
+// Pseudo ontologie : c'est plutôt un recueil de concepts vaguement rangés
+const raw = `	production
+		orm
+		logiciel
+		programme
+		source
+		framework
+		langage
+			java
+			c++
+			javascript
+			php
+			c#
+			python
+			cobol
+	modélisation
+		use-case
+		agilité
+		architecture
+	environnement
+		compilation
+		ide
+		bug
+		debugger
+		test
+		os
+		système d'exploitation
+	paradigme
+		fonctionnel
+		déclaratif
+		impératif
+			séquence
+			instruction
+		objet
+			classe
+			instance
+			interface
+	interface
+		embarqué
+		mobile
+		web
+		client lourd`;
+
+//--------------------------------------------------------------CONCEPT CLASS
+class Concept {
+  constructor(name, superConcept, subConcepts, level) {
+    this.name = name;
+    this.level = level;
+    this.superConcept = superConcept;
+    this.subConcepts = subConcepts;
+  }
+  toString() {
+    let s = "";
+    for (let i = 0; i < this.level; i++) s += "\t";
+    s += this.name + "\n";
+    this.subConcepts.forEach((sub) => (s += sub.toString()));
+    return s;
+  }
+  asList() {
+    let t = [];
+    t.push(this);
+    this.subConcepts.forEach((sub) => (t = t.concat(sub.asList())));
+    return t;
+  }
+}
+
+//------------------------------------------------------------PARSING
+const ontology = new Concept("informatique", null, [], 0);
+
+// Parsing pénible de la pseudo-ontologie (string)
+const stack = [ontology];
+const list = raw.split("\n");
+list.forEach((element) => {
+  let last = stack.at(-1);
+  let level = element.length - element.trim().length;
+  if (last.level < level) {
+    let concept = new Concept(element.trim(), last, [], level);
+    last.subConcepts.push(concept);
+    stack.push(concept);
+  } else if (last.level == level) {
+    stack.splice(stack.length - 1, 1);
+    last = stack.at(-1);
+    let concept = new Concept(element.trim(), last, [], level);
+    last.subConcepts.push(concept);
+    stack.push(concept);
+  } else {
+    // last.level > level
+    for (let i = 0; i < last.level - level; i++)
+      stack.splice(stack.length - 1, 1);
+    last = stack.at(-1);
+    let concept = new Concept(element.trim(), last, [], level);
+    last.subConcepts.push(concept);
+    stack.push(concept);
+  }
+});
+
+//-----------------------------------------------INDEXATION (UN PEU) SEMANTIQUE
+const concepts = ontology.asList();
+
+const matrix = {};
+const files = fs.readdirSync("./rome_txt/txt");
+
+files.forEach((file) => {
+  let content = fs.readFileSync("./rome_txt/txt/" + file, "utf8").toLowerCase();
+  // Remplacement des caractères de séparation par des espaces
+  content = content.replace(/[\n\t,.!-]/g, " ");
+  let vector = {};
+  matrix[file] = vector;
+  concepts.forEach((concept) => {
+    // Recherche des occurrences exactes en expressions individuelles
+    // (encadrées par des espaces)
+    if (content.indexOf(" " + concept.name + " ") != -1)
+      vector[concept.name] = 1;
+    else vector[concept.name] = 0;
+  });
+});
+
+//------------------------------------------------------------RESULTAT
+// Nombre de concepts trouvés par document, sans gestion de hiérarchie
+Object.keys(matrix).forEach((file) => {
+  let vector = matrix[file];
+  let sum = 0;
+  Object.values(vector).forEach((v) => (sum += v));
+  console.log(file, sum);
+});
+
+//---------------------------------------------TEMPS DE RECHERCHE
+const before = Date.now();
+
+const docs = [];
+Object.keys(matrix).forEach((file) => {
+  if (matrix[file]["python"] == 1) docs.push(file);
+});
+
+const after = Date.now();
+console.log(after - before);
+console.log(docs.length);
